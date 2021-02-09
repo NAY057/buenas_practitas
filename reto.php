63518aabf014f59050e0536a5f82f75538e435d3
Nicolas Aguirre Yacup <nicolas.aguirre.y@gmail.com>
2021-02-08 23:12:25 -0500

    cursos de buenas practicas de escritura de codigo reto 1

---
+++ reto.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <form action="reto2.php" method="post">
+        <input name="Numero"/>
+        <input type="submit"/>
+    </form>
+</body>
+</html>
+
+<?php
+    $numero = $_POST['Numero'];
+
+    function mostrarMultiplicacion($numero){
+        for($i = 0; $i <= 10; $i++){
+            echo'<tr>
+                    <td>' .$numero. 'x' .$i. '</td>
+                    <td>' .$numero*$i. '</td>
+                </tr>';
+        }
+    }
+
+    function factorizarNumero($numero){
+        $fatorial = 1;
+        for ($f = $numero; $f >= 1; $f--) {
+            $factorial *= $f;
+        }
+        mostrarFactorial($numero, $factorial);
+    }
+
+    function mostrarFactorial($numero, $factorialResultado){
+        echo'<tr>
+                <td>' .$numero. '</td>
+                <td>' .$factorialResultado. '</td>
+            </tr>';
+    }
+?>
+<table border="1">
+    <?php
+        mostrarFactorial($numeroPost);
+        factorizarNumero($numeroPost);
+            ?>
+</table>
